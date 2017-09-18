b007df52ded1a7fb882f183cea982c686adc9d85
Lance Wicks <lw@judocoach.com>
2017-09-18 10:51:50 +0100

    Actual add the waza list changes.

--- lib/Judo.pm
+++ lib/Judo.pm
@@ -31,6 +31,61 @@ method waza () {
                         name => 'Kata-guruma',
                         kanji => '肩車',
                     },
+                    sukui-nage => {
+                        number => 6,
+                        name => 'Sukui-nage',
+                        kanji => '掬投',
+                    },
+                    obi-otoshi => {
+                        number => 7,
+                        name => 'Obi-otoshi',
+                        kanji => '帯落',
+                    },
+                    uki-otoshi => {
+                        number => 8,
+                        name => 'Uki-otoshi',
+                        kanji => '浮落',
+                    },
+                    sumi-otoshi => {
+                        number => 9,
+                        name => 'Sumi-otoshi',
+                        kanji => '隅落',
+                    },
+                    yama-arashi => {
+                        number => 10,
+                        name => 'Yama-arashi',
+                        kanji => '山嵐',
+                    },
+                    obi-tori-gaeshi => {
+                        number => 11,
+                        name => 'Obi-tori-gaeshi',
+                        kanji => '帯取返',
+                    },
+                    morote-gari => {
+                        number => 12,
+                        name => 'Morote-gari',
+                        kanji => '双手刈'
+                    },
+                    kuchiki-taoshi => {
+                        number => 13,
+                        name => 'Kuchiki-taoshi',
+                        kanji => '朽木倒',
+                    },
+                    kibisu-gaeshi => {
+                        number => 14,
+                        name => 'Kibisu-gaeshi',
+                        kanji => '踵返',
+                    },
+                    uchi-mata-sukashi => {
+                        number => 15,
+                        name => 'Uchi-mata-sukashi',
+                        kanji => '内股すかし',
+                    },
+                    ko-uchi-gaeshi => {
+                        number => 16,
+                        name => 'Ko-uchi-gaeshi',
+                        kanji => '小内返'
+                    },
                 },
         },
 
